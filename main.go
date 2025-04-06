ecfa9a6283c18fc442e7f4f68c8d4d1b9e5981f3
Raunak Singh <raunaksingh@Raunaks-Air.lan>
2025-04-06 17:53:44 +0530

    1.1.6

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		// Use gib to generate a random string
 		randomString := "23"
-		c.String(200, fmt.Sprintf("Hello, World i want to check if the update sys is working! Here's a random string: %s", randomString))
+		c.String(200, fmt.Sprintf("Check 2 Hello, World i want to check if the update sys is working! Here's a random string: %s", randomString))
 	})
 
 	fmt.Println("Starting server on on on :8080...")
