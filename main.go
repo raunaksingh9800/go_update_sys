1549f216bbddc40f93a9452f9a2de4a09480b5e8
Raunak Singh <raunaksingh@Raunaks-Air.lan>
2025-04-06 14:14:53 +0530

    1.1.4

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		c.String(200, fmt.Sprintf("Hello, World! Here's a random string: %s", randomString))
 	})
 
-	fmt.Println("Starting server on :8080...")
+	fmt.Println("Starting server on on on :8080...")
 	err := r.Run(":8080")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
